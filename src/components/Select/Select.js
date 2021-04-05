e9609ced7249ababea4c120c3a736bf408f09921
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2021-04-04 23:14:50 -0400

    Build select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+
+const Wrapper = styled.select`
+  font-size: 1rem;
+  color: inherit;
+  padding: 12px 52px 12px 16px;
+  margin: 0 0 0 8px;
+  border: none;
+  border-radius: 8px;
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+`;
+
+const WrapperIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  color: currentColor;
+  margin: auto;
+  pointer-events: none;
+`;
+
+const ColorWrapper = styled.span`
+  position: relative;
+  color: ${COLORS.gray700};
+  :hover {
+    color: ${COLORS.black};
+  }
+`;
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <label>
+      {label}
+      <ColorWrapper>
+        <Wrapper value={value} onChange={onChange}>
+          {children}
+        </Wrapper>
+        <WrapperIcon
+          aria-hidden="true"
+          id="chevron-down"
+          size={24}
+          strokeWidth={2}
+        />
+      </ColorWrapper>
+    </label>
   );
 };
 
